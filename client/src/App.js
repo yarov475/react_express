9a9baee0ed3435d9df4d3115b764975bd31111e3
yarov475 <59488110+yarov475@users.noreply.github.com>
2021-04-07 13:33:43 +0300

    parallel working exp react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// On lines 6 to 9, we inserted a constructor, that initializes the default state.
+//     On lines 11 to 16, we inserted the method callAPI() that will fetch the data from the API and store the response on this.state.apiResponse.
+//     On lines 18 to 20, we inserted a react lifecycle method called componentDidMount(), that will execute the callAPI() method after the component mounts.
+//     Last, on line 29, I used the <;p> tag to display a paragraph on our client page, with the text that we retrieved from the API.
+//
+
+class App extends  Component{
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: `` };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+  render(){
+    return(
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} alt='logo' className={App-logo}/>
+<h1 className='App-intro'>  React + Express APP</h1>
+          </header>
+<p className='App-intro'>{this.state.apiResponse}</p>
+        </div>
+    );
+
+  }
+
 }
 
 export default App;
